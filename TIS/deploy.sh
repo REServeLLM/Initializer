7e0eee58d0ef08a5767340bd7562f4c3963b7ec4
ZHANG Yihui <zhangyihui@buaa.edu.cn>
2024-07-05 17:58:48 +0800

    Update script

--- TIS/deploy.sh
+++ TIS/deploy.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+### We have copy TensorRT-LLM and tensorrtllm_backend source code in image!
+### But it's better to pull the latest code before deploying to KServe cluster!
+
+
 set -e  # exit when any error happens
 
 # Setup ENV
+
+## HuggingFace Model in PVC Storage Path
 MODEL_REPO_DIR=/mnt/models/models/Meta-Llama-3-8B-Instruct
+
+## Source Code Path
+TRT_BACKEND_DIR=/code/tensorrtllm_backend
+TRT_LLM_DIR=/code/tensorrtllm_backend/TensorRT-LLM
+
+## Output Path
 OUTPUT_CKP_DIR=/workspace/checkpoints
 OUTPUT_ENGINE_DIR=/workspace/engines
-TRT_BACKEND_DIR=/workspace/tensorrtllm_backend
-TRT_LLM_DIR=/workspace/tensorrtllm_backend/TensorRT-LLM
 
-# Clone tensorrtllm_backend Repo
-echo "Clone tensorrtllm_backend repo..."
-if [ -d "$TRT_BACKEND_DIR" ]; then
-  echo "Directory tensorrtllm_backend exists, skip cloning"
-else
-  cd /root
-  git clone -b v0.10.0 https://
+
 
